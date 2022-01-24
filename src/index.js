a2a9c10c0af21a5cd4c92f553dd881cece1ab3d7
Andy Tan <andytan0727@gmail.com>
2022-01-24 20:43:47 +0800

    feat: scaffold pages (signup UI complete)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline";
 import "./index.css";
+
 import App from "./App";
+import SignUp from "./routes/SignUp";
+import Register from "./routes/Register";
+
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/* MUI setup */}
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <CssBaseline />
+
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
