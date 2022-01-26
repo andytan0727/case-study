897dd7fbe48b0da2dad750e6bb98a3c5c4c40dcc
Andy Tan <andytan0727@gmail.com>
2022-01-26 15:45:47 +0800

    feat(main): app bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import './App.css';
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import PeopleOutlineIcon from "@mui/icons-material/PeopleOutline";
+import Typography from "@mui/material/Typography";
+import { Link } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      Pending     
-    </div>
+    <Container>
+      <AppBar
+        position="static"
+        sx={{
+          mt: "1rem",
+          borderRadius: "20px",
+          color: "white",
+        }}
+      >
+        <Toolbar>
+          <PeopleOutlineIcon />
+          <Typography
+            variant="h6"
+            sx={{ flexGrow: 1, pl: "1rem", fontWeight: "normal" }}
+          >
+            Employee Management App
+          </Typography>
+          <Link to="/login" style={{ textDecoration: "none", color: "white" }}>
+            Logout
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Container>
   );
 }
 
